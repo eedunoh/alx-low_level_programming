c08a1c5aebcedff7c2e664e2b678fb5295c4f440
eedunoh <63975169+eedunoh@users.noreply.github.com>
2023-02-19 21:38:54 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,28 +1,29 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-
-int main(void)
 {
-int d;
+	int i = '0';
+	int j = '0';
 
-for (d = '0'; d < '100'; d++)
-{
-putchar((d / '10') + '0');
-putchar((d % '10') + '0');
-if (d != '99')
-{
-putchar(',');
-putchar(' ');
-}
-}
-putchar('\n');
-
-return (0);
+	while (i <= '9')
+	{
+		while (j <= '9')
+		{
+			if (!(i > j || i == j))
+			{
+				putchar(i);
+				putchar(j);
+				if (i == '8' && j == '9')
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			j++;
+		}
+		j = '0';
+		i++;
+	}
+	return (0);
 }

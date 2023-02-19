22021c7bb5379ae18af2212bc2a4cc580b99d00f
eedunoh <63975169+eedunoh@users.noreply.github.com>
2023-02-19 21:50:48 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,27 +8,27 @@
 
 int main(void)
 {
-int p, q;
+	int p, q;
 
-for (p = 0; p <= 98; p++)
-{
-for (q = p + 1; q <= 99; q++)
-{
-putchar((p / 10) + 0);
-putchar((p % 10) + 0);
-putchar( );
-putchar((q / 10) + 0);
-putchar((q % 10) + 0);
+	for (p = 0; p <= 98; p++)
+	{
+		for (q = p + 1; q <= 99; q++)
+		{
+			putchar((p / 10) + '0');
+			putchar((p % 10) + '0');
+			putchar(' ');
+			putchar((q / 10) + '0');
+			putchar((q % 10) + '0');
 
-if (p == 98 && q == 99)
-continue;
+			if (p == 98 && q == 99)
+				continue;
 
-putchar(,);
-putchar( );
-}
-}
+			putchar(',');
+			putchar(' ');
+		}
+	}
 
-putchar(n);
+	putchar('\n');
 
-return (0);
+	return (0);
 }

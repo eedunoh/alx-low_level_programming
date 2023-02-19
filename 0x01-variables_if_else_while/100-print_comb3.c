164c76e7211c7aa9165ae6d11884204a1716c4b8
eedunoh <63975169+eedunoh@users.noreply.github.com>
2023-02-19 21:36:22 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,17 +12,17 @@ int main(void)
 {
 int d;
 
-for (d = 0; d < 100; d++)
+for (d = '0'; d < '100'; d++)
 {
-putchar((d / 10) + 0);
-putchar((d % 10) + 0);
-if (d != 99)
+putchar((d / '10') + '0');
+putchar((d % '10') + '0');
+if (d != '99')
 {
-putchar(,);
-putchar( );
+putchar(',');
+putchar(' ');
 }
 }
-putchar(n);
+putchar('\n');
 
 return (0);
 }

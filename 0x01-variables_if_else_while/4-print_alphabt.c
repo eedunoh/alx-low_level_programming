f14ac76c3bb72a11d9631e7d943432971e7ca6c0
eedunoh <63975169+eedunoh@users.noreply.github.com>
2023-02-19 20:55:31 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,12 +10,12 @@ int main(void)
 {
 char low, e, q;
 
-e = e;
-q = q;
+e = 'e';
+q = 'q';
 
 for (low = 'a'; low <= 'z'; low++)
 {
-if (low != 'e' && low != 'q')
+if (low != e && low != q)
 putchar(low);
 }
 putchar('\n');

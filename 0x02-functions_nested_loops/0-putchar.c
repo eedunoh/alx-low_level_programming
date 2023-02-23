84f3457728e1418d520eec503bc4daf2badbe751
Edunoh <eedunoh@gmail.com>
2023-02-22 16:43:18 -0800

    -

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+
+{
+
+README.md _putchar(_);
+README.md _putchar(p);
+README.md _putchar(u);
+README.md _putchar(t);
+README.md _putchar(c);
+README.md _putchar(h);
+README.md _putchar(a);
+README.md _putchar(r);
+README.md _putchar(n);
+README.md return (0);
+
+}

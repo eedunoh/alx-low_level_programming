fe8f333cdcffcf4e12ab5291482f97aacb5a7700
Edunoh <eedunoh@gmail.com>
2023-02-28 19:03:59 -0800

    -

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -12,7 +12,7 @@ int main(void)
 
 {
 	char password[84];
-	int index = 0, sum = 0, diff_half1;
+	int index = 0, sum = 0, diff_half1, diff_half2;
 
 	srand(time(0));
 
@@ -28,15 +28,33 @@ int main(void)
 	if (sum != 2772)
 	{
 		diff_half1 = (sum - 2772) / 2;
+		diff_half2 = (sum - 2772) / 2;
 
-		if ((sum - 2772) % 2 != 0)					
+		if ((sum - 2772) % 2 != 0)
+		diff_half1++;			
 		
 		for (index = 0; password[index]; index++)
+			{
+				if (password[index] >= (33 + diff_half1))
+					{
+				password[index] -= diff_half1;
+								break;
+					}
 
-		password[index] -= diff_half1;
+			}
+		               
+	       
+		for (index = 0; password[index]; index++)
+                	{
+                                if (password[index] >= (33 + diff_half2))
+                                        {
+                                password[index] -= diff_half2;
+                                                                break;
+                                        }
+
+                 	}
 
 	}
-
 	printf("%s", password);
 	return (0);
 }
